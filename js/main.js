74b883e624086e2be97f304aaadab13b263d48be
CoderDi <lvbnhbq2103@mail.ru>
2020-03-02 01:38:38 +0700

    home

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
     $("#popup-call").show();
     $(".popup").addClass("popup--show");
   });
+  $(".js-city-change").click(function(){
+    $(".popup-block").hide();
+    $("#popup-city").show();
+    $(".popup").addClass("popup--show");
+  });
   $(".popup-close").click(function(){
     $(".popup").removeClass("popup--show");
   });
@@ -20,13 +25,128 @@ $(document).ready(function(){
     
   });
 
+  $(".js-input-search").on('input keyup', function(e) {
+    $(this).parents("form").find(".city-search__results").show();
+  });
+  $(".js-input-search").on('focusout', function(e) {
+    $(this).parents("form").find(".city-search__results").hide();
+  });
+  $(".js-cart-open").click(function() {
+    $(".cart").addClass("cart--open");
+  });
+  $(".js-cart-close").click(function() {
+    $(".cart").removeClass("cart--open");
+    $(".cart__btn").removeClass("cart__btn--active");
+  });
+
+  $(".cart__btn").click(function(){
+    $(".cart__btn").removeClass("cart__btn--active");
+    $(this).addClass("cart__btn--active");
+    $(".cart-tab-link").removeClass("cart-tab-link--active");
+    if ($(this).hasClass("cart__btn--cart")) {
+      $(".cart-block-cart").show();
+      $(".cart-block-wish").hide();
+      $(".cart-ready-link").addClass("cart-tab-link--active");
+    }
+    if ($(this).hasClass("cart__btn--wish")) {
+      $(".cart-block-cart").hide();
+      $(".cart-block-wish").show();
+      $(".cart-wish-link").addClass("cart-tab-link--active");
+    }
+  });
+
+  $(".cart-tab-link").click(function(){
+    $(".cart-tab-link").removeClass("cart-tab-link--active");
+    $(this).addClass("cart-tab-link--active");
+    $(".cart__btn").removeClass("cart__btn--active");
+    if ($(this).hasClass("cart-ready-link")) {
+      $(".cart-block-cart").show();
+      $(".cart-block-wish").hide();
+      $(".cart__btn--cart").addClass("cart__btn--active");
+    }
+    if ($(this).hasClass("cart-wish-link")) {
+      $(".cart-block-cart").hide();
+      $(".cart-block-wish").show();
+      $(".cart__btn--wish").addClass("cart__btn--active");
+    }
+  });
+
+  $(".cart-item__remove").click(function(){
+    var then = $(this).parents(".cart-item");
+    $(then).hide(200);
+    setTimeout(function () {
+      $(then).remove();
+    }, 200);
+  });
+
+  $(".js-search-open").click(function(){
+    $(".search").addClass("search--open");
+  });
+  $(".js-search-close").click(function(){
+    $(".search").removeClass("search--open");
+  });
+
+  $(".js-wish-add").click(function(){
+    $(this).toggleClass("active");
+  });
+  $(".js-compare-add").click(function(){
+    $(this).toggleClass("active");
+  });
+  $(".js-cart-add").click(function(){
+    $(this).addClass("active");
+    $(this).parents(".item-btn-container").addClass("active");
+  });
+
+  $(".js-filter-block-open").click(function(){
+    $(this).parents(".filter__block").toggleClass("active");
+    $(this).parents(".filter__block").find(".filter__block_content").slideToggle(200);
+  });
+
   
   $(".js-slider").slick({
-    infinite: false,
+    infinite: true,
     autoplay: true,
     autoplaySpeed: 6000,
-    arrows: false
-    
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+  var html5Slider = document.getElementById('price-slider'),
+      minValue = parseInt($(html5Slider).attr("data-min")),
+      maxValue = parseInt($(html5Slider).attr("data-max")),
+      step = parseInt($(html5Slider).attr("data-step"));
+
+  noUiSlider.create(html5Slider, {
+      start: [110, 545],
+      connect: true,
+      step: step,
+      range: {
+          'min': minValue,
+          'max': maxValue
+      }
+  });
+
+  var inputMinNumber = document.getElementById('price-slider__input--min'),
+  inputMaxNumber = document.getElementById('price-slider__input--max');
+
+  html5Slider.noUiSlider.on('update', function (values, handle) {
+
+      var value = values[handle];
+
+      if (handle) {
+          inputMaxNumber.value = Math.round(value);
+      } else {
+        inputMinNumber.value = Math.round(value);
+      }
+  });
+
+  inputMaxNumber.addEventListener('change', function () {
+      html5Slider.noUiSlider.set([null, this.value]);
+  });
+
+  inputMinNumber.addEventListener('change', function () {
+    html5Slider.noUiSlider.set([this.value, null]);
   });
   
   
@@ -44,68 +164,37 @@ $(document).ready(function(){
   });
 
   
-  ymaps.ready(function () {
-    if ($("#map").length != 0) {
-      var myMap = new ymaps.Map('map', {
-          center: [56.6362, 47.85],
-          zoom: 13,
-          controls: []
-        }),
-        myPlacemark = new ymaps.Placemark([56.635471, 47.837894], {
-          hintContent: 'улица Строителей, 19',
-          balloonContent: 'улица Строителей, 19'
-        }, {
-          iconLayout: 'default#image',
-          iconImageHref: 'images/pin.png',
-          iconImageSize: [41, 41],
-          iconImageOffset: [-20, -41]
-        }),
 
-        myPlacemark2 = new ymaps.Placemark([56.637838, 47.868302], {
-          hintContent: 'улица Машиностроителей, 61',
-          balloonContent: 'улица Машиностроителей, 61'
-        }, {
-          iconLayout: 'default#image',
-          iconImageHref: 'images/pin2.png',
-          iconImageSize: [41, 41],
-          iconImageOffset: [-20, -41]
-        });
-
-        myMap.geoObjects.add(myPlacemark);
-        myMap.geoObjects.add(myPlacemark2);
-        myMap.behaviors.disable('scrollZoom');
+  $(".counter-minus").click(function(){
+    var step = parseFloat($(this).parents(".counter").attr("data-step")),
+        current = parseFloat($(this).parents(".counter").find(".counter-value").val());
+    current -= step;
+    if (current < 0) {
+      current = 0;
     }
-    
+    $(this).parents(".counter").find(".counter-value").val(current.toFixed(1));
 
-    if ($("#map-popup").length != 0) {
-      var myMapPopup = new ymaps.Map('map-popup', {
-        center: [56.6362, 47.85],
-        zoom: 13,
-        controls: []
-      }),
-      myPlacemarkPopup = new ymaps.Placemark([56.635471, 47.837894], {
-        hintContent: 'улица Строителей, 19',
-        balloonContent: 'улица Строителей, 19'
-      }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'images/pin.png',
-        iconImageSize: [41, 41],
-        iconImageOffset: [-20, -41]
-      }),
+    var onePrice = parseFloat($(this).parents(".cart-item").find(".cart-item-price-one strong").text());
 
-      myPlacemarkPopup2 = new ymaps.Placemark([56.637838, 47.868302], {
-        hintContent: 'улица Машиностроителей, 61',
-        balloonContent: 'улица Машиностроителей, 61'
-      }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'images/pin2.png',
-        iconImageSize: [41, 41],
-        iconImageOffset: [-20, -41]
-      });
-      myMapPopup.geoObjects.add(myPlacemarkPopup);
-      myMapPopup.geoObjects.add(myPlacemarkPopup2);
-      myMapPopup.behaviors.disable('scrollZoom');
-    }
+    $(this).parents(".cart-item").find(".cart-item-price-full strong").text((onePrice * current).toFixed());
     
   });
+
+  $(".counter-plus").click(function(){
+    var step = parseFloat($(this).parents(".counter").attr("data-step")),
+        current = parseFloat($(this).parents(".counter").find(".counter-value").val());
+    current += step;
+    $(this).parents(".counter").find(".counter-value").val(current.toFixed(1));
+    var onePrice = parseFloat($(this).parents(".cart-item").find(".cart-item-price-one strong").text());
+
+    $(this).parents(".cart-item").find(".cart-item-price-full strong").text((onePrice * current).toFixed());
+  });
+  $(".counter-value").on("input keyup", function(){
+    var onePrice = parseFloat($(this).parents(".cart-item").find(".cart-item-price-one strong").text());
+
+    $(this).parents(".cart-item").find(".cart-item-price-full strong").text((onePrice * parseFloat($(this).val())).toFixed());
+    
+  });
+
+
 });
